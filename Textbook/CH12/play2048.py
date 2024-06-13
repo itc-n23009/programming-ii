4d7448b54bac799352bf566ef107fcb8be26aa80
itc-n23009 <n23009@std.it-college.ac.jp>
2024-06-13 01:22:55 +0000

    CH12

--- Textbook/CH12/play2048.py
+++ Textbook/CH12/play2048.py
@@ -1,16 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 
-GAME_URL = "https://play2048.co/"
+GAME_URL = "https://rugugu.jp/2048/"
 
 driver = webdriver.Chrome()
 
 try:
     driver.get(GAME_URL)
     time.sleep(2)
-    
-    game_board = driver.find_element_by_tag_name('body')
+
+    game_board = driver.find_element(By.TAG_NAME, 'body')
 
     while True:
         for action in [Keys.UP, Keys.RIGHT, Keys.DOWN, Keys.LEFT]:
@@ -18,8 +19,8 @@ try:
             time.sleep(0.1)
 
 except Exception as e:
-    print(f"An error occurred: {e}")
+    print(f"エラーが発生しました: {e}")
 
-finally:
-    driver.quit()
+
+
 
